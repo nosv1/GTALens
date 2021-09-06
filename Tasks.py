15cde28f7ac4f16f9be2d619e64a9025ec0a00b9
Chris Seagraves <christopher.seagraves@gmail.com>
2021-09-05 23:15:59 -0500

    Update Tasks.py

--- Tasks.py
+++ Tasks.py
@@ -120,9 +120,10 @@ async def update_jobs():
             creator_id IS NULL
     """
 
-    db.cursor.execute(f"""{delete_not_creators};""")
-    logger.debug(f"Deleted Members: {db.cursor.rowcount}")
-    db.connection.commit()
+    # FIXME apparently this takes long time again...
+    # db.cursor.execute(f"""{delete_not_creators};""")
+    # logger.debug(f"Deleted Members: {db.cursor.rowcount}")
+    # db.connection.commit()
 
     creators_limit = 6
     tbd_creators_limit = 4
