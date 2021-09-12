c79aac8b87b1c99834828a9f2fb80fe0b55e54e2
Chris Seagraves <christopher.seagraves@gmail.com>
2021-09-12 16:55:02 -0500

    Revert "Update Jobs.py"
    This reverts commit 4965fe920e3eb9ab07d99b99f5ae308e22d53428.

--- Jobs.py
+++ Jobs.py
@@ -314,30 +314,24 @@ async def add_sc_members(crew_json: json) -> None:
         for rank in crew_json['crewRanks']:
 
             for member in rank['rankMembers']:
-                while True:
-                    try:
-                        db.cursor.execute(f"""
-                            INSERT INTO members (
-                                _id, _name
-                            ) VALUES (
-                                '{member['rockstarId']}', '{member['nickname']}'
-                            ) ON DUPLICATE KEY UPDATE
-                                _name='{member['nickname']}' 
-                            ;""")
+                db.cursor.execute(f"""
+                    INSERT INTO members (
+                        _id, _name
+                    ) VALUES (
+                        '{member['rockstarId']}', '{member['nickname']}'
+                    ) ON DUPLICATE KEY UPDATE
+                        _name='{member['nickname']}' 
+                    ;""")
 
-                        primary_crew = member['primaryClan']
-                        db.cursor.execute(f"""
-                            INSERT INTO crews (
-                                _id, _name
-                            ) VALUES (
-                                '{primary_crew['id']}', '{replace_chars(primary_crew['name'])}' 
-                            ) ON DUPLICATE KEY UPDATE
-                                _name='{replace_chars(primary_crew['name'])}'
-                            ;""")
-                    except mysql.connector.errors.DatabaseError:
-                        # deadlock found when trying to get lock; try restarting transaction :shrug:
-                        logger.warning("mysql.connector.errors.DatabaseError, trying again in 1 second")
-                        await asyncio.sleep(1)
+                primary_crew = member['primaryClan']
+                db.cursor.execute(f"""
+                    INSERT INTO crews (
+                        _id, _name
+                    ) VALUES (
+                        '{primary_crew['id']}', '{replace_chars(primary_crew['name'])}' 
+                    ) ON DUPLICATE KEY UPDATE
+                        _name='{replace_chars(primary_crew['name'])}'
+                    ;""")
 
     db.connection.commit()
     db.connection.close()
