351c705875e3cfb65018a90edfb58bd0a4bced26
Chris Seagraves <christopher.seagraves@gmail.com>
2021-09-25 04:16:58 -0500

    Update Jobs.py

--- Jobs.py
+++ Jobs.py
@@ -779,7 +779,7 @@ async def send_possible_jobs(
 
             possible_jobs_str += f"\n{Support.LETTERS_EMOJIS[letters[i]]} " \
                                  f"{platform_emoji} " \
-                                 f"[{job.name}](https://gtalens.com/job/{job.rockstar_id}) - " \
+                                 f"[{job.name.replce(']', '\\]')}](https://gtalens.com/job/{job.rockstar_id}) - " \
                                  f"[{creator.name}](https://gtalens.com/profile/{job.creator.id})"
 
             embed_meta += f"{Support.LETTERS_EMOJIS[letters[i]]}={job.rockstar_id}/"
